af56942bb9a1b6f3f31fe7d1ab8315037a2af1b1
박준형 <pjun22.dev@gmail.com>
2026-09-21 19:30:39 +0900

    Fix benchmark runner

--- performance/benchmark.ps1
+++ performance/benchmark.ps1
@@ -1,21 +1,17 @@
-$ErrorActionPreference$mysql$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
+$ErrorActionPreference = 'Stop'
+$mysql = 'C:\Program Files\MariaDB 11.4\bin\mysql.exe'
 if (-not $env:BENCHMARK_DB_PASSWORD) { throw 'Set BENCHMARK_DB_PASSWORD before running.' }
-$passwordArg$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
-$mysql$mysql$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
-if (-not $env:BENCHMARK_DB_PASSWORD) { throw 'Set BENCHMARK_DB_PASSWORD before running.' }
-$passwordArg$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
-$db$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
-$queries$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
-  pagination$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
-  movie_id_join$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
-  rating_sort$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
-  leading_wildcard$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
-  comment_count$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
+$db = @('-h','127.0.0.1','-P','3307','-u','root',('-p' + $env:BENCHMARK_DB_PASSWORD),'projectdb','-N')
+$queries = [ordered]@{
+  pagination = "SELECT SQL_NO_CACHE r.review_id FROM bench_reviews r JOIN bench_movies m ON m.movie_id=r.movie_id JOIN bench_users u ON u.user_id=r.user_id ORDER BY r.created_at DESC LIMIT 20 OFFSET 50000"
+  movie_id_join = "SELECT SQL_NO_CACHE r.review_id FROM bench_reviews r JOIN bench_movies m ON m.movie_id=r.movie_id JOIN bench_users u ON u.user_id=r.user_id WHERE r.movie_id=42 ORDER BY r.created_at DESC LIMIT 20"
+  rating_sort = "SELECT SQL_NO_CACHE r.review_id FROM bench_reviews r JOIN bench_movies m ON m.movie_id=r.movie_id JOIN bench_users u ON u.user_id=r.user_id ORDER BY r.rating DESC LIMIT 20"
+  leading_wildcard = "SELECT SQL_NO_CACHE m.title,r.content FROM bench_reviews r JOIN bench_movies m ON m.movie_id=r.movie_id WHERE m.title LIKE '%Movie 42%'"
+  comment_count = "SELECT SQL_NO_CACHE r.review_id,(SELECT COUNT(*) FROM bench_comments c WHERE c.review_id=r.review_id) FROM bench_reviews r ORDER BY r.created_at DESC LIMIT 20"
 }
 function MeasureQuery($label, $query) {
-  $times$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
-  $median$db = @('-h','127.0.0.1','-P','3307','-u','root',$passwordArg,'projectdb','-N')
+  $times = 1..5 | ForEach-Object { [math]::Round((Measure-Command { & $script:mysql @script:db -e $query | Out-Null }).TotalMilliseconds, 1) }
+  $median = ($times | Sort-Object)[2]
   "${label}: $($times -join ', ') ms; median=$median ms"
 }
-
 foreach ($name in $queries.Keys) { MeasureQuery $name $queries[$name] }
